8e5bb723ef09553913782b86ee18e87682f86be7
jossanchcald <jossanch@espol.edu.ec>
2026-08-12 14:10:27 -0500

    Frangmentos de plantillas y SSR

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,4 +7,8 @@ def home(request):
     return render(request, 'dashboard/base.html')
 
 def index(request):
-    return render(request, 'dashboard/index.html')
+    data = {
+        'title': "Landing Page' Dashboard",
+    }
+
+    return render(request, 'dashboard/index.html', data)
